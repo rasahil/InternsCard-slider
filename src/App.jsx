66ef29e39f60d99b076c7e9d5e5aa0fc4936e657
rasahil <sahilrangari31@gmail.com>
2024-05-14 19:12:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Slider from './Components/Slider'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <div className='bg-cyan-400 container-fluid  w-100'>
+        <Slider/>
+      </div>
+    </>
+  )
+}
+
+export default App
